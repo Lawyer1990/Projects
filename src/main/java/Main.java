6b51182854dda09a0acee8243a8401d23fbb825f
Alexey <alexey.sklutov@mail.ru>
2021-09-27 14:02:05 +0300

    Projects

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        test1();
+        hello();
+        test();
+    }
+    public static void test1() {
+        int [] numbers = new int[5];
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        try{
+            for (int i=0;i<numbers.length;i++){
+                System.out.print("Введите значение массива:");
+                numbers[i] = input.nextInt();
+                if (numbers[i]%3==0 & numbers[i]!=0) arrayList.add(numbers[i]);
+            }
+            System.out.println(arrayList.toString());
+        } catch (Exception e){
+            System.out.println("Значение некорректное");
+        }
+    }
+
+    public static void hello() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число больше 7:");
+        try {
+            int number = input.nextInt();
+            if (number>7) System.out.println("Привет");
+        } catch (Exception e) {
+            System.out.println("Значение некорректное");
+        }
+    }
+    public static void test() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите имя:");
+        try {
+            String name = input.next();
+            if (name.equals("Вячеслав")) {
+                System.out.println("Привет, Вячеслав");
+            } else System.out.println("Нет такого имени");
+
+        } catch (Exception e) {
+            System.out.println("Значение некорректное");
+        } finally {
+            input.close();
+        }
+    }
+}
+
+    /*Дана скобочная последовательность: [((())()(())]] - Можно ли считать эту последовательность правильной? - Если ответ на предыдущий вопрос “нет” - то что необходимо в ней изменить, чтоб она стала правильной?
+
+        Ответ: 1. Нет, она не правильная.2. Необходимо добавить скобки следующим образом [[((())()(()))]]
+        */
